b56ecb481be9987ea999a7d0a77d9a94b57fe5cc
Paul Gi <paulsgi@PAUL-MAC.local>
2019-03-14 10:17:33 -0700

    single quote to double quote

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Example extends Component {
   render() {
     return (
-      <div className='Example'>
+      <div className="Example">
         <h1>Hello, World!</h1>
       </div>
     );
